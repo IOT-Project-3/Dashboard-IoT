6f633bf3339bae4843e5294499c0a903d8334916
Cameron <cameron.bonsigne@epita.fr>
2025-11-18 11:35:17 +0100

    installation shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './styles/App.css'
+import { Button } from '@/components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+        <Button onClick={() => alert('Ça marche !')}>
+            Cliquez-moi
+        </Button>
     </>
   )
 }
