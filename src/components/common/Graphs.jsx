18c9def5febbc843c93397e1a5ab8083dcdaa230
Desooe-pro <sacha.allardin06@gmail.com>
2025-11-24 16:57:15 +0100

    Fonctionnel ?

--- src/components/common/Graphs.jsx
+++ src/components/common/Graphs.jsx
@@ -2,7 +2,14 @@
 
 import React, { useEffect, useState } from "react";
 import { TrendingUp } from "lucide-react";
-import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  ReferenceLine,
+  XAxis,
+  YAxis,
+} from "recharts";
 import {
   Card,
   CardContent,
@@ -16,18 +23,192 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button.jsx";
+import { NumToMois } from "@/functions/GestionnaireDates.jsx";
 
-function Graphs({ chartData }) {
+function Graphs({ chartData, line = null }) {
+  const [ChartData, setChartData] = useState(chartData);
   const [trends, setTrends] = useState(null);
   const [chartConfig, setChartConfig] = useState(null);
   const [params, setParams] = useState(null);
   const [yAxisConfigs, setYAxisConfigs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [hardData, setHardData] = useState({
+    "Aujourd'hui": [
+      { heure: "05", féquentation: 1 },
+      { heure: "06", féquentation: 3 },
+      { heure: "07", féquentation: 6 },
+      { heure: "08", féquentation: 10 },
+      { heure: "09", féquentation: 12 },
+      { heure: "10", féquentation: 14 },
+      { heure: "11", féquentation: 18 },
+      { heure: "12", féquentation: 34 },
+      { heure: "13", féquentation: 45 },
+      { heure: "14", féquentation: 36 },
+      { heure: "15", féquentation: 19 },
+      { heure: "16", féquentation: 24 },
+      { heure: "17", féquentation: 16 },
+      { heure: "18", féquentation: 11 },
+      { heure: "19", féquentation: 9 },
+      { heure: "20", féquentation: 6 },
+      { heure: "21", féquentation: 2 },
+    ],
+    Mois: {
+      année: {
+        2024: {
+          Décembre: [
+            { jour: "01", fréquentation: 92 },
+            { jour: "02", fréquentation: 108 },
+            { jour: "03", fréquentation: 86 },
+            { jour: "04", fréquentation: 78 },
+            { jour: "05", fréquentation: 80 },
+            { jour: "06", fréquentation: 50 },
+            { jour: "07", fréquentation: 42 },
+            { jour: "08", fréquentation: 55 },
+            { jour: "09", fréquentation: 56 },
+            { jour: "10", fréquentation: 76 },
+          ],
+        },
+        2025: {
+          Janvier: [
+            { jour: "01", fréquentation: 92 },
+            { jour: "02", fréquentation: 600 },
+            { jour: "03", fréquentation: 86 },
+            { jour: "04", fréquentation: 78 },
+            { jour: "05", fréquentation: 80 },
+            { jour: "06", fréquentation: 50 },
+            { jour: "07", fréquentation: 42 },
+            { jour: "08", fréquentation: 55 },
+            { jour: "09", fréquentation: 56 },
+            { jour: "10", fréquentation: 76 },
+          ],
+          Février: [
+            { jour: "01", fréquentation: 92 },
+            { jour: "02", fréquentation: 700 },
+            { jour: "03", fréquentation: 86 },
+            { jour: "04", fréquentation: 78 },
+            { jour: "05", fréquentation: 80 },
+            { jour: "06", fréquentation: 50 },
+            { jour: "07", fréquentation: 42 },
+            { jour: "08", fréquentation: 55 },
+            { jour: "09", fréquentation: 56 },
+            { jour: "10", fréquentation: 76 },
+          ],
+          Mars: [
+            { jour: "01", fréquentation: 92 },
+            { jour: "02", fréquentation: 800 },
+            { jour: "03", fréquentation: 86 },
+            { jour: "04", fréquentation: 78 },
+            { jour: "05", fréquentation: 80 },
+            { jour: "06", fréquentation: 50 },
+            { jour: "07", fréquentation: 42 },
+            { jour: "08", fréquentation: 55 },
+            { jour: "09", fréquentation: 56 },
+            { jour: "10", fréquentation: 76 },
+          ],
+          Avril: [
+            { jour: "01", fréquentation: 92 },
+            { jour: "02", fréquentation: 900 },
+            { jour: "03", fréquentation: 86 },
+            { jour: "04", fréquentation: 78 },
+            { jour: "05", fréquentation: 80 },
+            { jour: "06", fréquentation: 50 },
+            { jour: "07", fréquentation: 42 },
+            { jour: "08", fréquentation: 55 },
+            { jour: "09", fréquentation: 56 },
+            { jour: "10", fréquentation: 76 },
+          ],
+          Octobre: [
+            { jour: "01", fréquentation: 92 },
+            { jour: "02", fréquentation: 108 },
+            { jour: "03", fréquentation: 86 },
+            { jour: "04", fréquentation: 78 },
+            { jour: "05", fréquentation: 80 },
+            { jour: "06", fréquentation: 50 },
+            { jour: "07", fréquentation: 42 },
+            { jour: "08", fréquentation: 55 },
+            { jour: "09", fréquentation: 56 },
+            { jour: "10", fréquentation: 76 },
+          ],
+        },
+      },
+    },
+    Année: {
+      2024: [
+        { mois: "January", fréquentation: 186 },
+        { mois: "February", fréquentation: 305 },
+        { mois: "March", fréquentation: 237 },
+        { mois: "April", fréquentation: 73 },
+        { mois: "May", fréquentation: 209 },
+        { mois: "June", fréquentation: 214 },
+      ],
+      2025: [
+        { mois: "January", fréquentation: 186 },
+        { mois: "February", fréquentation: 305 },
+        { mois: "March", fréquentation: 237 },
+        { mois: "April", fréquentation: 73 },
+        { mois: "May", fréquentation: 209 },
+        { mois: "June", fréquentation: 214 },
+      ],
+    },
+  });
+  const [currentSelection, setCurrentSelection] = useState(null);
+
+  async function getDataFromHardData(key, annee = null, mois = null) {
+    let tempDatas = null;
+    const date = new Date();
+    if (key === "Année") {
+      if (annee === null) {
+        annee = date.getFullYear();
+      }
+      tempDatas = hardData[key][annee];
+      await setCurrentSelection([key, Object.keys(hardData[key]), annee]);
+    } else if (key === "Mois") {
+      if (annee === null) {
+        annee = date.getFullYear();
+      }
+      if (mois === null) {
+        mois = NumToMois(date.getMonth());
+      }
+      tempDatas = hardData[key]["année"][annee][mois];
+      let moisDatas = {};
+      const moisKeys = Object.keys(hardData[key]["année"]);
+      for (let i = 0; i < moisKeys.length; i++) {
+        moisDatas[moisKeys[i]] = Object.keys(
+          hardData[key]["année"][moisKeys[i]],
+        );
+      }
+      await setCurrentSelection([key, moisKeys, moisDatas, annee, mois]);
+    } else {
+      tempDatas = hardData[key];
+      await setCurrentSelection(key);
+    }
+    await setChartData(tempDatas);
+    await setIsLoading(true);
+  }
 
   function getTrendAndPercentage(tocheck) {
-    const len = chartData.length - 1;
+    let len = ChartData.length - 1;
+    console.log(ChartData[len][tocheck]);
+    while (ChartData[len][tocheck] === ChartData[len - 1][tocheck]) {
+      len -= 1;
+    }
     const percentage = (
-      (chartData[len][tocheck] / chartData[len - 1][tocheck]) * 100 -
+      (ChartData[len][tocheck] / ChartData[len - 1][tocheck]) * 100 -
       100
     ).toFixed(2);
     const trend = percentage > 0 ? "up" : "down";
@@ -49,16 +230,16 @@ function Graphs({ chartData }) {
   }
 
   function generateYAxisConfig(param) {
-    let max = chartData[0][param];
-    let min = chartData[0][param];
+    let max = ChartData[0][param];
+    let min = ChartData[0][param];
 
     // Trouver min et max
-    for (let i = 0; i < chartData.length; i++) {
-      if (chartData[i][param] < min) {
-        min = chartData[i][param];
+    for (let i = 0; i < ChartData.length; i++) {
+      if (ChartData[i][param] < min) {
+        min = ChartData[i][param];
       }
-      if (chartData[i][param] > max) {
-        max = chartData[i][param];
+      if (ChartData[i][param] > max) {
+        max = ChartData[i][param];
       }
     }
 
@@ -87,12 +268,12 @@ function Graphs({ chartData }) {
   }
 
   function generateConfig() {
-    const keys = Object.keys(chartData[0]);
+    const keys = Object.keys(ChartData[0]);
     let config = {};
     let param = {
       XAxis: keys[0],
       datas: [],
-      amountOf: chartData.length,
+      amountOf: ChartData.length,
     };
 
     for (let i = 1; i < keys.length; i++) {
@@ -133,16 +314,16 @@ function Graphs({ chartData }) {
       const trendsData = generateAxisTrend(param.datas);
       const yConfigs = generateAllYAxisConfigs(param.datas);
 
-      setChartConfig(config);
-      setParams(param);
-      setTrends(trendsData);
-      setYAxisConfigs(yConfigs);
-      setIsLoading(false);
+      await setChartConfig(config);
+      await setParams(param);
+      await setTrends(trendsData);
+      await setYAxisConfigs(yConfigs);
+      await setIsLoading(false);
     }
     void fetchData();
-  });
+  }, [ChartData]);
 
-  if (!chartData || !chartData[0] || Object.keys(chartData[0]).length < 1) {
+  if (!ChartData || !ChartData[0] || Object.keys(ChartData[0]).length < 1) {
     return <div>No data</div>;
   }
 
@@ -153,22 +334,145 @@ function Graphs({ chartData }) {
   return (
     <Card className="Card">
       <CardHeader>
-        <CardTitle>Area Chart - Linear</CardTitle>
-        <CardDescription>
-          Showing total of {params.datas[0]}{" "}
-          {params.datas.length > 1 ? "and " + params.datas[1] : ""}{" "}
-          {" for the last " +
-            params.amountOf +
-            " " +
-            params.XAxis +
-            (params.amountOf > 1 ? "s" : "")}
+        <CardTitle className={"flex flex-row-reverse"}>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button
+                type="button"
+                className={
+                  "h-9 px-4 py-2 has-[>svg]:px-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+                }
+              >
+                {currentSelection !== null
+                  ? typeof currentSelection === "object"
+                    ? currentSelection[0]
+                    : currentSelection
+                  : "Ouvrir"}
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="start">
+              <DropdownMenuGroup>
+                {Object.keys(hardData).map((key) => {
+                  if (key !== currentSelection) {
+                    return (
+                      <DropdownMenuItem
+                        onClick={async () => getDataFromHardData(key)}
+                        key={key}
+                      >
+                        {key}
+                      </DropdownMenuItem>
+                    );
+                  }
+                })}
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </CardTitle>
+        <CardDescription className={"flex justify-between"}>
+          <div>
+            Montre : {params.datas[0]}{" "}
+            {params.datas.length > 1 ? "et " + params.datas[1] : ""}{" "}
+            {" au cours des " +
+              params.amountOf +
+              " derniers " +
+              params.XAxis +
+              (params.amountOf > 1 &&
+              params.XAxis.substring(params.XAxis.length - 1) !== "s"
+                ? "s"
+                : "")}
+          </div>
+          <div>
+            {currentSelection === null || currentSelection === "Aujourd'hui" ? (
+              ""
+            ) : (
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <button
+                    type="button"
+                    className={
+                      "h-9 px-4 py-2 has-[>svg]:px-3 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50"
+                    }
+                  >
+                    {currentSelection[0] === "Année" ? currentSelection[2] : ""}
+                    {currentSelection[0] === "Mois"
+                      ? currentSelection[4] + " - " + currentSelection[3]
+                      : ""}
+                  </button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-56" align="start">
+                  <DropdownMenuGroup>
+                    {currentSelection[1].map((key) => {
+                      if ("Année" === currentSelection[0]) {
+                        console.log(currentSelection, key);
+                        if (parseInt(currentSelection[2]) !== parseInt(key)) {
+                          return (
+                            <DropdownMenuItem
+                              onClick={async () =>
+                                getDataFromHardData(currentSelection[0], key)
+                              }
+                              key={key}
+                            >
+                              {key}
+                            </DropdownMenuItem>
+                          );
+                        }
+                      } else if ("Mois" === currentSelection[0]) {
+                        console.log(currentSelection);
+                        return (
+                          <div>
+                            <DropdownMenuLabel key={key}>
+                              {key}
+                            </DropdownMenuLabel>
+                            <DropdownMenuGroup>
+                              {currentSelection[2][key].map((mois) => {
+                                console.log(currentSelection);
+                                if (
+                                  parseInt(currentSelection[3]) ===
+                                    parseInt(key) &&
+                                  currentSelection[4] === mois
+                                ) {
+                                  return "";
+                                } else {
+                                  return (
+                                    <DropdownMenuItem
+                                      onClick={async () =>
+                                        getDataFromHardData(
+                                          currentSelection[0],
+                                          key,
+                                          mois,
+                                        )
+                                      }
+                                      key={key + "-" + mois}
+                                    >
+                                      {mois}
+                                    </DropdownMenuItem>
+                                  );
+                                }
+                              })}
+                            </DropdownMenuGroup>
+                            {currentSelection[1][
+                              currentSelection[1].length - 1
+                            ] !== key ? (
+                              <DropdownMenuSeparator />
+                            ) : (
+                              ""
+                            )}
+                          </div>
+                        );
+                      }
+                    })}
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            )}
+          </div>
         </CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig} className="GraphContainer">
           <AreaChart
             accessibilityLayer
-            data={chartData}
+            data={ChartData}
             margin={{
               left: 12,
               right: 12,
@@ -197,6 +501,16 @@ function Graphs({ chartData }) {
                 />
               );
             })}
+            {line !== null ? (
+              <ReferenceLine
+                yAxisId={params.datas[0]}
+                y={line}
+                stroke="red"
+                strokeDasharray="5 0"
+              />
+            ) : (
+              ""
+            )}
             <ChartTooltip
               cursor={false}
               content={<ChartTooltipContent indicator="dot" hideLabel />}
@@ -237,7 +551,10 @@ function Graphs({ chartData }) {
         <div className="flex w-full items-start gap-2 text-sm">
           <div className="grid gap-2">
             {params.datas.map((param, key) => {
-              if (trends[param].trend === "up") {
+              if (
+                trends[param].trend === "up" &&
+                chartConfig[param] !== undefined
+              ) {
                 return (
                   <div
                     key={key}
@@ -248,7 +565,7 @@ function Graphs({ chartData }) {
                     <TrendingUp className="h-4 w-4" />
                   </div>
                 );
-              } else {
+              } else if (chartConfig[param] !== undefined) {
                 return (
                   <div
                     key={key}
--- src/pages/ToutVoir.jsx
+++ src/pages/ToutVoir.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-import GestionPermissions from "@/pages/GestionPermissions.jsx";
+import Graphs from "@/components/common/Graphs.jsx";
 
 function ToutVoir() {
   return (
     <>
-      <h1>Page tout voir</h1>
+      <Graphs
+        chartData={[
+          { mois: "January", fréquentation: 186, vent: 10 },
+          { mois: "February", fréquentation: 305, vent: 20 },
+          { mois: "March", fréquentation: 237, vent: 15 },
+          { mois: "April", fréquentation: 73, vent: 20 },
+          { mois: "May", fréquentation: 209, vent: 22 },
+          { mois: "June", fréquentation: 214, vent: 26 },
+        ]}
+        line={350}
+      />
     </>
   );
 }
