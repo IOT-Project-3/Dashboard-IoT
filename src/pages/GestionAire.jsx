200a1f09c8098616b22fa8aa2eab389c2a02687b
Desooe-pro <sacha.allardin06@gmail.com>
2025-11-26 17:00:41 +0100

    Reste à faire la connexion API

--- src/pages/GestionAire.jsx
+++ src/pages/GestionAire.jsx
@@ -30,17 +30,7 @@ function GestionAire() {
     <div>
       <CardsList cards={cards} />
       <div className={"pt-4"}>
-        <Graphs
-          chartData={[
-            { mois: "January", fréquentation: 186, vent: 10 },
-            { mois: "February", fréquentation: 305, vent: 20 },
-            { mois: "March", fréquentation: 237, vent: 15 },
-            { mois: "April", fréquentation: 73, vent: 20 },
-            { mois: "May", fréquentation: 209, vent: 22 },
-            { mois: "June", fréquentation: 214, vent: 26 },
-          ]}
-          line={350}
-        />
+        <Graphs />
       </div>
     </div>
   );
