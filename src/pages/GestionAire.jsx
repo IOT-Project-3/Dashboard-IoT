8ad1ffb0ea9cd0e6130fad470afc1e01dcac1323
Desooe-pro <sacha.allardin06@gmail.com>
2025-11-27 10:37:06 +0100

    Modifs post merge

--- src/pages/GestionAire.jsx
+++ src/pages/GestionAire.jsx
@@ -1,28 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import CardsList from "@/components/common/CardsList.jsx";
 import { Home } from "lucide-react";
 import Graphs from "@/components/common/Graphs.jsx";
 import PageTitle from "../components/common/PageTitle";
 
-const cards = [
-  {
-    icone: Home,
-    titre: "Dernière mesure",
-    texte: "4,18m",
-  },
-  {
-    icone: Home,
-    titre: "Date et heure de la mesure",
-    texte: "20/10/2025 14:27",
-  },
-  {
-    icone: Home,
-    titre: "État global",
-    texte: "Critique",
-  },
-];
-
 function GestionAire() {
+  const cards = [
+    {
+      icone: Home,
+      titre: "Dernière mesure",
+      texte: "4,18m",
+    },
+    {
+      icone: Home,
+      titre: "Date et heure de la mesure",
+      texte: "20/10/2025 14:27",
+    },
+    {
+      icone: Home,
+      titre: "État global",
+      texte: "Critique",
+    },
+  ];
   return (
     <>
       {/* Nom de la page */}
@@ -35,16 +34,7 @@ function GestionAire() {
         </div>
 
         {/* Graph */}
-        <Graphs
-          chartData={[
-            { month: "January", visites: 186, wind: 70 },
-            { month: "February", visites: 305, wind: 50 },
-            { month: "March", visites: 237, wind: 20 },
-            { month: "April", visites: 73, wind: 60 },
-            { month: "May", visites: 209, wind: 20 },
-            { month: "June", visites: 214, wind: 10 },
-          ]}
-        />
+        <Graphs />
       </div>
     </>
   );
